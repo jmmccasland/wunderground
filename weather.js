903f1d654f7ac8c3e09feeaea6e5c683e2f39d5e
John Michael McCasland <jmmccasland@gmail.com>
2017-07-13 11:36:19 -0500

    Can log current temp in inputted city

--- weather.js
+++ weather.js
@@ -1,2 +1,28 @@
 const https = require('https');
 const api = require('./api.json');
+
+// Print out temperature details
+function printMessage(location, temp) {
+  console.log(`The current temperature in ${location} is ${temp}.`);
+}
+
+// Print out error Messages
+
+
+
+function get(query) {
+  const request = https.get(`https://api.wunderground.com/api/${api.key}/conditions/q/${query}.json`, response => {
+      let body = "";
+
+      response.on('data', chunk => {
+        body += chunk.toString();
+      });
+
+      response.on('end', () => {
+        const profile = JSON.parse(body);
+        printMessage(profile.current_observation.display_location.full, profile.current_observation.temperature_string)
+      });
+  });
+}
+
+module.exports.get = get;
