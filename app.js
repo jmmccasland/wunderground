a5b9f38a6b5c01cf141ac4339cf0fc539e19f125
John Michael McCasland <jmmccasland@gmail.com>
2017-07-13 11:14:57 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const weather = require('./weather');
+// Join multiple values passed as arguments and replace all spaces with underscores
+const query = process.argv.slice(2).join("_").replace(" ", "_");
+weather.get(query);
